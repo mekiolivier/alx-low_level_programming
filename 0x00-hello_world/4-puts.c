04de0312f13832d7d0f86e72ee5075e9002afdb4
Mekila <mekiolivier@gmail.com>
2022-06-05 15:41:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 /* * 
  * main - Entry point
@@ -10,9 +10,10 @@
 int main(void)
 
 {
-       puts("\"Programming is like building a multilingual puzzle");
+       
+	puts("\"Programming is like building a multilingual puzzle");
+
 
 return (0);
-
 
 }
