95f5255edf0e6212cb477e84c4c23f665c351d06
Mekila <mekiolivier@gmail.com>
2022-06-05 15:22:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-/*
+/* * 
+ * main - Entry point
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
+ * Return: always 0 (success)
  *
  */
 
 int main(void)
 
 {
-puts("\"Programming is like building a multilingual puzzle");
+       puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
+
 
 }
