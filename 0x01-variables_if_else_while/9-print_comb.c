3b99c3c54163e3bf3cc6459af4be4917807b19c2
Mekila <mekiolivier@gmail.com>
2022-06-12 08:26:14 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 int i;
-for (i = 48; i <= 58; i++)
+for (i = '0'; i <= '9' ; i++)
 {
 putchar(i);
-}
-if (i != 57)
-{	
+if (i != '9')
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
