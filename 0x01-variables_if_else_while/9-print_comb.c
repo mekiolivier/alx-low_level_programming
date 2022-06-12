48e074fd939179fe85414a84a1975604b8641639
Mekila <mekiolivier@gmail.com>
2022-06-12 08:01:26 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *    main - Entry point
- *    Return: Always 0 (Success)
+ *   main - Entry point
+ *   Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -9,11 +9,11 @@ int i;
 for (i = 48; i <= 58; i++)
 {
 putchar(i);
+}
 if (i != 57)
 {	
 putchar(',');
 putchar(' ');
-}
 }
 putchar('\n');
 return (0);
